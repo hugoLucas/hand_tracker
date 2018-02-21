25df0f7266c92c55ff3b8cd88cbed1cd02aea6e5
hugoLucas <hugojlucas@gmail.com>
2018-02-20 19:12:41 -0800

    Updated gen_examples

--- data_gen/gen_examples.py
+++ data_gen/gen_examples.py
@@ -6,38 +6,38 @@ import os
 from data_gen.constants import TIME_LIMIT, FRAME_OFFSET
 
 # What should we label these images as?
-LABEL = 'open_palm'
+LABEL = 'ok_sign'
 
 # Read frames from webcam while user does hand gestures
-# cap = cv.VideoCapture(1)
-# start, diff = time.process_time(), 0
-# frames, counter = [], 0
-#
-# while diff < TIME_LIMIT:
-#     ret, frame = cap.read()
-#
-#     diff = time.process_time() - start
-#     cv.imshow('OUTPUT', frame)
-#     if cv.waitKey(1) & 0xFF == ord('q'):
-#         break
-#
-#     if counter + 1 == FRAME_OFFSET:
-#         frames.append(frame)
-#     counter = (counter + 1) % FRAME_OFFSET
-#
-# cap.release()
+cap = cv.VideoCapture(0)
+start, diff = time.process_time(), 0
+frames, counter = [], 0
+
+while diff < TIME_LIMIT:
+    ret, frame = cap.read()
+
+    diff = time.process_time() - start
+    cv.imshow('OUTPUT', frame)
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    if counter + 1 == FRAME_OFFSET:
+        frames.append(frame)
+    counter = (counter + 1) % FRAME_OFFSET
+
+cap.release()
 
 # Ask the user to draw bounding boxes around hand
-# boxes, current = [], 0
-# print('TOTAL: ', len(frames))
-# for frame in frames:
-#     bbox = cv.selectROI("Image", frame, False, False)
-#     if cv.waitKey(1) & 0xFF == ord('q'):
-#         print('Skip!')
-#     else:
-#         boxes.append([bbox[0], bbox[1], bbox[0] + bbox[2], bbox[1] + bbox[3]])
-#         print('DONE: ', current)
-#         current += 1
+boxes, current = [], 0
+print('TOTAL: ', len(frames))
+for frame in frames:
+    bbox = cv.selectROI("Image", frame, False, False)
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        print('Skip!')
+    else:
+        boxes.append([bbox[0], bbox[1], bbox[0] + bbox[2], bbox[1] + bbox[3]])
+        print('DONE: ', current+1)
+        current += 1
 
 # Save the boxes in a .csv file and frames to folder
 root_directory = './images'
@@ -51,20 +51,21 @@ if not os.path.exists(image_directory):
 csv_path = os.path.join(image_directory, '{}.csv'.format(LABEL))
 file_exists = os.path.exists(csv_path)
 
-FRAME_NUM = 0
-with open(csv_path, 'x') as csv_file:
+FRAME_NUM = sum(1 for line in open(csv_path, 'a+'))
+with open(csv_path, 'a') as csv_file:
+
     headers = ['image_name', 'box']
     writer = csv.DictWriter(csv_file, fieldnames=headers)
 
     if not file_exists:
         writer.writeheader()
 
-    # for frame, box in zip(frames, boxes):
-    #     frame_name = 'frame{}.jpg'.format(FRAME_NUM)
-    #     cv.imwrite('./{}/{}'.format(LABEL, frame_name), frame)
-    #
-    #     writer.writerow({
-    #         'image_name': frame_name,
-    #         'box': box
-    #     })
-    #     FRAME_NUM += 1
+    for frame, box in zip(frames, boxes):
+        frame_name = 'frame_{}.jpg'.format(FRAME_NUM)
+        cv.imwrite(os.path.join(image_directory, frame_name), frame)
+
+        writer.writerow({
+            'image_name': frame_name,
+            'box': box
+        })
+        FRAME_NUM += 1
