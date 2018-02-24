c77c5b24aff07d54a0041dfa77afc7daf37e1893
hugolucas <hugojlucas@gmail.com>
2018-02-23 18:16:54 -0800

    data gen draft one complete, need to test against trainer

--- hand_sign_model/data_loader/data_generator.py
+++ hand_sign_model/data_loader/data_generator.py
@@ -1,12 +1,37 @@
+import tensorflow as tf
 import numpy as np
+
+import hand_sign_model.utils.config as conf
 
 
 class DataGenerator():
     def __init__(self, config):
         self.config = config
-        # load data here
-        self.input = np.ones((500, 784))
-        self.y = np.ones((500, 10))
+        self.dataset = tf.data.TFRecordDataset(filenames=config.train_tfrecord)
+        self.dataset = self.dataset.map(self.load_image_from_record)
+        self.dataset.repeat(None)
+        self.dataset = self.dataset.shuffle(buffer_size=config.num_elements)
+
     def next_batch(self, batch_size):
-        idx = np.random.choice(500, batch_size)
-        yield self.input[idx], self.y[idx]
+        batch = self.dataset.batch(batch_size)
+        iterator = batch.make_one_shot_iterator()
+        images, labels = iterator.get_next()
+        yield images, labels
+
+
+    @staticmethod
+    def load_image_from_record(serialized):
+        features = {
+            'label': tf.FixedLenFeature([], tf.string),
+            'img': tf.FixedLenFeature([], tf.string)
+        }
+        parsed_example = tf.parse_single_example(serialized, features)
+
+        image = parsed_example['img']
+        image = tf.decode_raw(image, tf.uint8)
+        image = tf.cast(image, tf.float32)
+
+        label = parsed_example['label']
+
+        return image, label
+
